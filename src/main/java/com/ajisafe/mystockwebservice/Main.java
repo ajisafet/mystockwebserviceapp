965f1b653dfe91fdf33cf416a5a410e3a99e4316
Temitope Ajisafe <ajisafet@gmail.com>
2019-10-30 04:31:29 +0000

    Stop HTTP server from stopping abruptly in the kubernetes pod

--- src/main/java/com/ajisafe/mystockwebservice/Main.java
+++ src/main/java/com/ajisafe/mystockwebservice/Main.java
@@ -38,8 +38,16 @@ public class Main {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.shutdownNow();
+        //System.in.read();
+        //server.shutdownNow();
+        try {
+			Thread.currentThread().join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			server.shutdownNow();
+		}
     }
 }
 
